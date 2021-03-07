12c5fe5e7913c4a9e99579291cf6dacd58e3ac18
sadlylifeofficical <79993075+sadlylifeofficical@users.noreply.github.com>
2021-03-08 02:29:42 +0700

    Update help.js

--- others/help.js
+++ others/help.js
@@ -16,7 +16,7 @@ module.exports = {
     let commands = message.client.commands.array();
  
     let helpEmbed = new MessageEmbed()
-      .setTitle("Harmony Help")
+      .setTitle("${client.user.uesrname} Help")
       .setAuthor('Click here to invite me!!', 'https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif', 'https://discord.com/api/oauth2/authorize?client_id=767885987740254291&permissions=49572160&scope=bot')
       .setDescription(`**PREFIX:** \`${PREFIX}\``)
       .setFooter( client.user.username +`Type: ${PREFIX}help <Command>  for more information!`, "https://cdn.discordapp.com/attachments/778600026280558617/781024479623118878/ezgif.com-gif-maker_1.gif")
