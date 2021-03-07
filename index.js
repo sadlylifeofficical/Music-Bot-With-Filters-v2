e1cf58367c10571981e7d6954307c1f553ddb50e
sadlylifeofficical <79993075+sadlylifeofficical@users.noreply.github.com>
2021-03-08 01:42:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.on(`ready`, () => {
       });
       
 
-      client.user.setActivity(`${PREFIX}help | ${client.guilds.cache.size} Server`, { type: "STREAMING",
+      client.user.setActivity(`${PREFIX}help | ${client.guilds.cache.size} Server`, { type: "PLAYING",
       url: "https://www.twitch.tv/nocopyrightsounds"});
    
   
